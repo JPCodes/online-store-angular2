3d4d69606c09723da9bffe37f8744e253281e5ad
Jim Padilla <jp_code2013@yahoo.com>
2017-02-19 14:36:06 -0800

    Add instructional text to make the app easier to use

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyBqSNmtvoKUGXYdmEXDyl12hin90GG0VNs",
+  authDomain: "online-store-4aa45.firebaseapp.com",
+  databaseURL: "https://online-store-4aa45.firebaseio.com",
+  storageBucket: "online-store-4aa45.appspot.com",
+  messagingSenderId: "1089419887981"
+};
